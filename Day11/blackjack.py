cfd4985ed5898e4c2e5901c74c7a9e06b8f6a1ad
Viktoria Silver <viktoria.sudraba@gmail.com>
2023-08-28 21:50:48 +0100

    Day11 Project: Blackjack - formatted style

--- Day11/blackjack.py
+++ Day11/blackjack.py
@@ -14,8 +14,8 @@ from random import randint
 
 ############### Our Blackjack House Rules #####################
 
-## The deck is unlimited in size. 
-## There are no jokers. 
+## The deck is unlimited in size.
+## There are no jokers.
 ## The Jack/Queen/King all count as 10.
 ## The the Ace can count as 11 or 1.
 ## Use the following list as the deck of cards:
@@ -40,8 +40,10 @@ from random import randint
 
 ###############################################################
 
+
 def draw_random():
     return cards[randint(0, 12)]
+
 
 def draw_again():
     again = input("Type 'y' to get another card, type 'n' to pass: ")
@@ -54,121 +56,119 @@ def draw_again():
     else:
         print("Wrong input. Try again")
 
+
 def special_sum(some_hand: list):
     if sum(some_hand) < 22 or 11 not in some_hand:
         return sum(some_hand)
-    return sum(some_hand)-10
+    return sum(some_hand) - 10
+
 
 def gone_over(hand: list):
     if special_sum(hand) > 21:
         return True
     return False
-    
-def  end_game(both_hands:dict):
-    #reveal computer's hand
-    print(f"Dealer's cards: {both_hands['deal_hand']}, score: {special_sum(both_hands['deal_hand'])}")
 
-    #user overrun their hand - automatic loss
+
+def end_game(both_hands: dict):
+    # reveal computer's hand
+    print(
+        f"Dealer's cards: {both_hands['deal_hand']}, score: {special_sum(both_hands['deal_hand'])}"
+    )
+
+    # user overrun their hand - automatic loss
     if special_sum(both_hands["my_hand"]) > 21:
-        sys.exit ("You lose. Game over")
-    #user aced - loose if computer also aced, otherwise win
+        sys.exit("You lose. Game over")
+    # user aced - loose if computer also aced, otherwise win
     elif special_sum(both_hands["my_hand"]) == 21:
         if special_sum(both_hands["deal_hand"]) == 21:
-            sys.exit  ("You lose. Game over")
+            sys.exit("You lose. Game over")
         else:
-            sys.exit ("You win!")
-    #user didn't ace - compare scores
+            sys.exit("You win!")
+    # user didn't ace - compare scores
     else:
-        #computer overrun - user wins
+        # computer overrun - user wins
         if special_sum(both_hands["deal_hand"]) > 21:
-            sys.exit ("You win!")
-        #computer aced - user looses
+            sys.exit("You win!")
+        # computer aced - user looses
         elif special_sum(both_hands["deal_hand"]) == 21:
-            sys.exit ("You lose. Game over")
-        #user scored higher than computer - user wins
+            sys.exit("You lose. Game over")
+        # user scored higher than computer - user wins
         elif special_sum(both_hands["my_hand"]) > special_sum(both_hands["deal_hand"]):
-            sys.exit ("You win!")
-        #user scored less than computer - user looses
+            sys.exit("You win!")
+        # user scored less than computer - user looses
         else:
-            sys.exit ("You lose. Game over")
-
+            sys.exit("You lose. Game over")
 
 
 def round(counter, hands):
-
     # print(f"BTW, counter is {counter}")
     # print(f"BTW, my hand: {hands['my_hand']}")
     # print(f"BTW, deal hand: {hands['deal_hand']}")
 
-    #draws card to each hand twice
+    # draws card to each hand twice
     if counter < 2:
         hands["my_hand"].append(draw_random())
         hands["deal_hand"].append(draw_random())
-        counter +=1
+        counter += 1
         round(counter, hands)
 
-    #user's rounds
-    elif counter < 12: #theoretically user could get 2 * 10 before overruning
-        #tell the user his hand and first card in computer's hand
-        print(f"Your cards: {hands['my_hand']}, current score: {special_sum(hands['my_hand'])}")
-        
-        #check if user gone over - end game if they have
-        if gone_over(hands['my_hand']):
+    # user's rounds
+    elif counter < 12:  # theoretically user could get 2 * 10 before overruning
+        # tell the user his hand and first card in computer's hand
+        print(
+            f"Your cards: {hands['my_hand']}, current score: {special_sum(hands['my_hand'])}"
+        )
+
+        # check if user gone over - end game if they have
+        if gone_over(hands["my_hand"]):
             end_game(hands)
-        #if user aced - end game    
+        # if user aced - end game
         if special_sum(hands["my_hand"]) == 21:
             end_game(hands)
 
         print(f"Dealer's first card: {hands['deal_hand'][0]}")
 
-
-        #ask user if they want to draw again
-        next_round, = helper_functions.defensive_while(draw_again)
-        #if user want to draw, then draw and go to next round
+        # ask user if they want to draw again
+        (next_round,) = helper_functions.defensive_while(draw_again)
+        # if user want to draw, then draw and go to next round
         if next_round:
             hands["my_hand"].append(draw_random())
-            counter +=1
+            counter += 1
             round(counter, hands)
         # if user does not want to draw, go to computer's rounds
         else:
             counter = 13
             round(counter, hands)
 
-    #computer's rounds
+    # computer's rounds
     else:
-        #if computer aced - end game
+        # if computer aced - end game
         if special_sum(hands["deal_hand"]) == 21:
-            end_game(hands)        
-        #if computer's hand less than 17, draw again
-        elif special_sum(hands["deal_hand"]) <17:
+            end_game(hands)
+        # if computer's hand less than 17, draw again
+        elif special_sum(hands["deal_hand"]) < 17:
             hands["deal_hand"].append(draw_random())
             round(counter, hands)
-        #else - end game
+        # else - end game
         else:
             end_game(hands)
 
 
-
-#--------------------------------
+# --------------------------------
 # MAIN FUNCTION STARTS HERE
-#--------------------------------
+# --------------------------------
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-both_hands = {
-    "my_hand" : [],
-    "deal_hand" : []
-}
+both_hands = {"my_hand": [], "deal_hand": []}
 
 response = input("Do you want to play a game of Blackjack? type 'y' or 'n': ")
-if response.lower() == 'n':
+if response.lower() == "n":
     sys.exit("OK, nevermind.")
-if response.lower() !='y':
+if response.lower() != "y":
     sys.exit("I'll take it as a No.")
 
 clear()
 print(logo)
 
-#round counter
-counter = 0 
+# round counter
+counter = 0
 round(counter, both_hands)
-
-
