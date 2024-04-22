4de6e526758de432f46c8e286604d256638c6133
Viktoria Silver <viktoria.sudraba@gmail.com>
2024-04-22 18:34:46 +0100

    Day 2 Exercises - complete

---
+++ beginner/day_002-data_types/data_types.py
@@ -0,0 +1,9 @@
+two_digit_number = input()
+# 🚨 Don't change the code above 👆
+####################################
+# Write your code below this line 👇
+
+first_number = int(two_digit_number[0])
+second_number = int(two_digit_number[1])
+sum = first_number + second_number
+print(sum)
---
+++ beginner/day_002-data_types/life_in_weeks.py
@@ -0,0 +1,7 @@
+age = input()
+# 🚨 Don't change the code above 👆
+# Write your code below this line 👇
+
+years_left = 90 - int(age)
+weeks_left = years_left * 52
+print(f"You have {weeks_left} weeks left.")
