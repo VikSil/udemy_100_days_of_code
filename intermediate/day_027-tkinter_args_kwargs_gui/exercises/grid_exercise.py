72f9a4286c0e689435a822ff182f15444e42beef
Viktoria Silver <viktoria.sudraba@gmail.com>
2024-05-02 18:27:25 +0100

    Day 27 Exercises - complete

---
+++ intermediate/day_027-tkinter_args_kwargs_gui/exercises/grid_exercise.py
@@ -0,0 +1,34 @@
+from tkinter import Tk, Label, Button, Entry
+
+
+window = Tk()
+window.title('First GUI program')
+window.minsize(width=500, height=300)
+window.config(padx=20, pady=20)
+
+label = Label(text="This is a label", font=('Corier New', 24, 'italic'))
+label.grid(column= 0, row = 0)
+
+def button_click():
+    new_button['text'] = 'They never listen.'
+    label['text'] = textbox.get()
+
+
+new_button = Button(text='Do not click Me!', command=button_click)
+new_button.grid(column=2, row = 0)
+new_button.config(padx = 50, pady = 50)
+
+
+def button_click():
+    button['text'] = 'Clickedy click'
+    label['text'] = textbox.get()
+
+button = Button(text='Click Me!', command=button_click)
+button.grid(column = 1, row = 1 )
+
+
+textbox = Entry(width=10)
+textbox.grid(column = 3, row = 3)
+
+
+window.mainloop()
---
+++ intermediate/day_027-tkinter_args_kwargs_gui/exercises/tkinter_exercises.py
@@ -0,0 +1,31 @@
+from tkinter import *
+
+
+window =Tk()
+window.title('First GUI program')
+window.minsize(width = 500, height = 300)
+
+label = Label(text = "This is a label", font = ('Corier New', 24, 'italic'))
+label.pack() # places the element into the window
+
+label['text'] = 'Changed text'
+label.config(text = 'Changed text again')
+
+
+def button_click():
+    button['text'] = 'Clickedy click'
+    label['text'] = textbox.get()
+
+button = Button(text = 'Click Me!', command = button_click)
+button.place(x=20, y=35)
+
+
+textbox = Entry(width = 10)
+textbox.pack()
+
+# grid system is relative to other components
+# it does not work with .pack()
+# textbox.grid(column=1, row = 1)
+
+
+window.mainloop()
